7332f1029eba4d2f67928e7b3b049b8293d55543
Parra117117 <102137724+Parra117117@users.noreply.github.com>
2024-10-27 22:15:09 -0600

    Update conexionBdd.php

--- controlador/conexionBdd.php
+++ controlador/conexionBdd.php
@@ -1,8 +1,11 @@
 <?php
     require('vendor/autoload.php');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
     
     function conectar(){
-        $cnn = mysqli_connect("localhost", "root", "", "cagie", "3306");
+        $cnn = mysqli_connect($_ENV['host'], $_ENV['user'], $_ENV['pwd'], $_ENV['bdd'],
+                            $_ENV['port']);
         $cnn -> set_charset("utf8");
 
         if (!$cnn) {
