36e5454d593b822fb26877731b62eb073b94ebd6
Parra117117 <102137724+LuisParra117@users.noreply.github.com>
2025-03-24 10:13:45 -0600

    Update controladorPlantel.php

--- controlador/controladorPlantel.php
+++ controlador/controladorPlantel.php
@@ -5,7 +5,7 @@
 
     if($accion == 'buscar'){
         $plantel = mysqli_real_escape_string(conectar(),$_GET['plantel']);
-        $query =   "SELECT latitud, longitud, calle, numeroExterior, numeroInterior, codigoPostal, colonia, localidad, estado 
+        $query =   "SELECT calle, numeroExterior, numeroInterior, codigoPostal, colonia, localidad, estado 
                     FROM planteles 
                     WHERE clavePlantel = '" . $plantel . "';";
         $result = mysqli_query(conectar(),$query);
@@ -13,8 +13,6 @@
         if($row = mysqli_fetch_array($result)){
 
             $datosEncontrados = array(
-                'latitud'           => $row['latitud'],
-                'longitud'          => $row['longitud'],
                 'calle'             => $row['calle'],
                 'numeroExterior'    => $row['numeroExterior'],
                 'numeroInterior'    => $row['numeroInterior'],
@@ -31,8 +29,6 @@
         echo json_encode($datosEncontrados);
     }else{
         $plantel =  mysqli_real_escape_string(conectar(),$_POST['plantel']);
-        $latitud =  mysqli_real_escape_string(conectar(),$_POST['latitud']);
-        $longitud =  mysqli_real_escape_string(conectar(),$_POST['longitud']);
         $calle =  mysqli_real_escape_string(conectar(),$_POST['calle']);
         $numeroExterior =  mysqli_real_escape_string(conectar(),$_POST['numeroExterior']);
         $numeroInterior =  mysqli_real_escape_string(conectar(),$_POST['numeroInterior']);
@@ -42,20 +38,20 @@
         $estado =  mysqli_real_escape_string(conectar(),$_POST['estado']);
         $nuevoPlantel = $_POST['nuevoPlantel'];
     
-        if($nuevoPlantel == 'true'){
-            $query = "INSERT INTO planteles (plantel, latitud, longitus, calle, numeroExterior, numeroInterior, codigoPostal, 
+        if ($nuevoPlantel == 'true') {
+            $query = "INSERT INTO planteles (plantel, calle, numeroExterior, numeroInterior, codigoPostal, 
                         colonia, localidad, estado)
-                        VALUES ('" . $plantel . "','" . $latitud . "','" . $longitud . "','" . $calle . "','" . $numeroExterior . 
+                        VALUES ('" . $plantel . "','" . $calle . "','" . $numeroExterior . 
                         "','" . $numeroInterior . "','" . $codigoPostal . "','" . $colonia . "','" . $localidad . 
                         "','" . $estado . "');";
-        }else{
-            $query =   "UPDATE planteles 
-                        SET latitud = '" . $latitud . "', longitud = '" . $longitud . "', calle = '" . $calle .
+        } else {
+            $query = "UPDATE planteles 
+                        SET calle = '" . $calle .
                         "', numeroExterior = '" . $numeroExterior . "', numeroInterior = '" . $numeroInterior . "', 
                         codigoPostal = '" . $codigoPostal . "', colonia = '" . $colonia . "', localidad = '" . $localidad . 
                         "', estado = '" . $estado . "' 
                         WHERE clavePlantel = '" . $plantel . "';";
-        }
+        }        
     
         $result = mysqli_query(conectar(),$query);
     
